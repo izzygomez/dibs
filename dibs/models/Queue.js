771b544b5ac47a18802d36a8e3c866e68051c780
dlernz <dlerner@mit.edu>
2015-11-20 13:46:55 -0500

    some methods for Queue model implemented..more to come

--- dibs/models/Queue.js
+++ dibs/models/Queue.js
@@ -11,6 +11,50 @@ var queueSchema = new Schema({
 	orders: [{type: Number, ref: 'Order'}]
 });
 
+
+userSchema.statics.getUser = function(username, callback)
+{
+  var result;
+  User.findOne({username: username}, function(err, user)
+  {
+    if (user == null)
+    {
+      result = null;
+    }
+    else
+    {
+      result = user
+    }
+    callback(result);
+  });
+}
+
+queueSchema.statics.getEventOrders = function(queueID, callback){
+	var result;
+	Queue.findOne({_id: queueID}, function(err, queue){
+		if (queue == null){
+			result = null;
+		}
+		else{
+			result = queue.orders
+		}
+		callback(result)
+	});
+}
+
+queueSchema.statics.getDrinkOrder = function(orders, orderID){
+	orders.foreach(function(order, orderID){
+		if (order._id == orderID){
+			return order
+		}
+	})
+}
+
+queueSchema.statics.addDrinkOrder = function(callback)
+{
+	var result;
+}
+
 // TODO create private and public methods
 // e.g. "var f = function (...) {...}" is private;
 //      "[schema].statics.f = function (...) {...}" is public
