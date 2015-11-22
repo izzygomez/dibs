edb086fdf0f05aaf50f08eac400fce402c631cae
Ismael Gomez <izzygmit@gmail.com>
2015-11-22 16:51:09 -0500

    modified app.js for FB login functionality; updated req'd packages; added config/ files

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+// get all the tools we need
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var dashboard = require('./routes/dashboard');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// required for passport
+app.use(session({ secret: 'derpaderpyderpderp' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +31,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// load in routes
+var index = require('./routes/index');
+var users = require('./routes/users');
+var dashboard = require('./routes/dashboard');
+require('./routes/auth')(app, passport);
+
+// setup routes
+app.use('/', index);
 app.use('/users', users);
 app.use('/dashboard', dashboard);
 
---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1001497043231909', // your App ID
+        'clientSecret'  : '13637ecafd3141c65a548a02cb7ca9cf', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    }
+    
+};
