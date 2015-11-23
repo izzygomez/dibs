a70dcc446c5be54001d3e495f4529f6417e13cce
dlernz <dlerner@mit.edu>
2015-11-22 23:42:43 -0500

    res.render function applied in routes for menu.js

--- routes/menus.js
+++ routes/menus.js
@@ -5,14 +5,16 @@ var router = express.Router();
 var utils = require('../utils/utils');
 var Menu = require('../models/Menu');
 
-
-router.get('/', function(req, res){
-	Menu.getMenu(menuID, function(err, menu){
-		if (err){
-			utils.sendErrResponse(res, 500, 'An unknown error occurred.');
-		} else{
-			utils.sendSuccessResponse(res, {menu: menu});
-		}
-	});
+router.get('/', function(req, res) {	
+  var menu;
+  Menu.getMenu(req.menuID, function(result)
+  {
+    menu = result;
+    res.render('menu', {eventName: 'Event Name', menu: menu}); 
+  })
 });
+
+
+
+
 
