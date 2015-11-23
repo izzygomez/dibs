40e8b13dd7e34585ed24253bf6709d389aecec7e
dlernz <dlerner@mit.edu>
2015-11-22 20:44:28 -0500

    queue route draft finished

--- routes/queue.js
+++ routes/queue.js
@@ -3,14 +3,43 @@ var router = express.Router();
 var utils = require('../utils/utils');
 var Queue = require('../models/Queue');
 
+/**
+POST /users/follows
+  Request body:
+    - username of user to follow
+    - username of current signed in user
+  Response:
+    - success: true if target user follow succeeded; false otherwise
+    - err: on error, an error message
+**/
 router.post('/served', function(req, res) {
   Queue.getNextOrder(req.body._id,
     function(err) {
       if (err) {
-        utils.sendErrResponse(res, 500, 'An unknown error has occurred.');
+        if (err.taken) {
+          utils.sendErrResponse(res, 400, 'That Queue is already created!');
+        } else {
+          utils.sendErrResponse(res, 500, 'An unknown error has occurred.');
+        }
       } 
       else {
         utils.sendSuccessResponse(res, req.body._id);
       }
   });
+
+  router.post('/create', function(req, res) {
+
+  Queue.createQueue(req.body._id,
+    function(err) {
+      if (err) {
+        if (err.taken) {
+          utils.sendErrResponse(res, 400, 'That Queue is already created!');
+        } else {
+          utils.sendErrResponse(res, 500, 'An unknown error has occurred.');
+        }
+      } else {
+        utils.sendSuccessResponse(res, req.body._id);
+      }
+  });
+});
 });
