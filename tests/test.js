fa1147a749b75aaa90cf531f2a616cfe5ebea5a5
dlernz <dlerner@mit.edu>
2015-12-06 16:58:37 -0500

    mocha added to package.json

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,15 @@
 var assert = require("assert");
-var Event = require('../models/Event');
-var User = require('../models/User');
-var Menu = require('../models/Menu');
-var Order = require('../models/Order');
-var Queue = require('../models/Queue');
+// var Event = require('../models/Event');
+// var User = require('../models/User');
+// var Menu = require('../models/Menu');
+// var Order = require('../models/Order');
+// var Queue = require('../models/Queue');
 
-// 
-describe('Menu', function()
-{
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
